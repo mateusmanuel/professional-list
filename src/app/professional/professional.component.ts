99f770960a159f9af5fb9129eb8b395d5ea2baa4
Mateus Manuel <mateusmanuel@unb.br>
2019-11-26 10:19:59 -0300

    creating professional module

---
+++ src/app/professional/professional.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatTable } from '@angular/material';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+
+export interface UsersData {
+  name: string;
+  id: number;
+}
+
+const ELEMENT_DATA: UsersData[] = [
+  {id: 1560608769632, name: 'Artificial Intelligence'},
+  {id: 1560608796014, name: 'Machine Learning'},
+  {id: 1560608787815, name: 'Robotic Process Automation'},
+  {id: 1560608805101, name: 'Blockchain'}
+];
+
+@Component({
+  selector: 'app-professional',
+  templateUrl: './professional.component.html',
+  styleUrls: ['./professional.component.css']
+})
+export class ProfessionalComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'name', 'action'];
+  dataSource = ELEMENT_DATA;
+
+  ngOnInit() {
+
+  }
+
+  @ViewChild(MatTable, { static: true }) table: MatTable<any>;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(action, obj) {
+    obj.action = action;
+    const dialogRef = this.dialog.open(DialogBoxComponent, {
+      width: '250px',
+      data: obj
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.event === 'Add') {
+        this.addRowData(result.data);
+      } else if (result.event === 'Update') {
+        this.updateRowData(result.data);
+      } else if (result.event === 'Delete') {
+        this.deleteRowData(result.data);
+      }
+    });
+  }
+
+  addRowData(rowObj) {
+    const d = new Date();
+    this.dataSource.push({
+      id: d.getTime(),
+      name: rowObj.name
+    });
+    this.table.renderRows();
+
+  }
+  updateRowData(rowObj) {
+    this.dataSource = this.dataSource.filter((value, key) => {
+      if (value.id === rowObj.id) {
+        value.name = rowObj.name;
+      }
+      return true;
+    });
+  }
+  deleteRowData(rowObj) {
+    this.dataSource = this.dataSource.filter((value, key) => {
+      return value.id !== rowObj.id;
+    });
+  }
+
+}
---
+++ src/app/professional/professional.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+
+import {
+  MatTableModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule
+} from '@angular/material';
+
+import { ProfessionalComponent } from './professional.component';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+
+@NgModule({
+  declarations: [
+    ProfessionalComponent,
+    DialogBoxComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  entryComponents: [
+    DialogBoxComponent
+  ],
+})
+export class ProfessionalModule { }
